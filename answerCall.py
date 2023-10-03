f15a595a1a51a23dc190e6c402aec202f3cd5df0
Erkson_Ronin <erksr226@hsd.k12.or.us>
2023-10-03 09:53:35 -0700

    getting started

--- answerCall.py
+++ answerCall.py
@@ -1,14 +1,17 @@
 # answerCall.py
-# by _______
+# by Ronin Erkson
 
 # For instructions on what to do, see README.md
 # or visit (https://github.com/HundredVisionsGuy/answerCall)
 
 # Write function defintion: answerCall()
-
+def answerCall(caller_code, sameAreaCode, cur_time):
+    response = True
+    hour, minute = cur_time.split(":")
+    return response
 # Make sure it returns a value
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
-    pass # remove or comment out this line if you wish to test the function
+    answerCall("U", False, "09:00")
