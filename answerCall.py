00e6700d523e60af8fab21bf7bb3f3dd7b5be3df
GitSomeBetchis <erksr226@hsd.k12.or.us>
2023-10-09 10:04:23 -0700

    getting started

--- answerCall.py
+++ answerCall.py
@@ -4,14 +4,14 @@
 # For instructions on what to do, see README.md
 # or visit (https://github.com/HundredVisionsGuy/answerCall)
 
-# Write function defintion: answerCall()
+
 def answerCall(caller_code, sameAreaCode, cur_time):
     response = True
-    hour, minute = cur_time.split(":")
     return response
-# Make sure it returns a value
+
+def test_answerCallForUnknownDifferentAreaCodeGoodTime():
+    response = screener.answerCall("U", False, "14:00")
+    return response == False
 
 if __name__ == '__main__':
-    # Call the function in here if you want to test it
-    # Make sure it's indented
     answerCall("U", False, "09:00")
